d826f94c1f3a49e0e168933fdf8d96b3a482e8b2
Fernando Felicio <fernandofelicio.dev@gmail.com>
2024-02-07 14:13:30 -0300

    Creating EmployeeClass

---
+++ src/entities/Employee.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Employee {
+	
+	private String name;
+	private Integer hours;
+	private Double value;
+	
+	public Employee() {
+		super();
+		
+	}
+	
+	public Employee (String name, Integer hours, Double value) {
+		super();
+		this.name = name;
+		this.hours = hours;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	public Double payment() {
+		return this.value * this.hours;
+	}
+
+}
